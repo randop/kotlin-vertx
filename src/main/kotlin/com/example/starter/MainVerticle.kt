459525a3b9588a9a3c51f28f72af2ca741d207c4
Randolph <randop@me.com>
2020-04-01 15:14:01 +0800

    MySQL database connectivity

--- src/main/kotlin/com/example/starter/MainVerticle.kt
+++ src/main/kotlin/com/example/starter/MainVerticle.kt
@@ -3,9 +3,17 @@ package com.example.starter
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
 
+import io.vertx.kotlin.sqlclient.*
+import io.vertx.mysqlclient.MySQLConnectOptions
+import io.vertx.mysqlclient.MySQLPool
+import io.vertx.sqlclient.Pool
+import io.vertx.sqlclient.PoolOptions
+
 class MainVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>) {
+
+
     vertx
       .createHttpServer()
       .requestHandler { req ->
@@ -17,6 +25,30 @@ class MainVerticle : AbstractVerticle() {
         if (http.succeeded()) {
           startPromise.complete()
           println("HTTP server started on port 8888")
+
+          val connectOptions = MySQLConnectOptions()
+              .setPort(3306)
+              .setHost("192.168.186.192")
+              .setDatabase("gateway")
+              .setUser("root")
+              .setPassword("mysql")
+              .setCharset("utf8mb4")
+              .setCollation("utf8mb4_unicode_ci")
+
+          val client = MySQLPool.pool(vertx, connectOptions, PoolOptions().setMaxSize(5))
+          client.query("SELECT CURRENT_TIMESTAMP()", { ar ->
+            if (ar.failed()) {
+              System.err.println("Cannot connect")                
+              ar.cause().printStackTrace()         
+              throw java.lang.RuntimeException(ar.cause())
+            }
+            if (ar.succeeded()) {
+              println("Database Connected: ${ar.result().first()}")                
+            } else {
+              println("Database Connection Failure: ${ar.cause()}")
+            }
+          })
+
         } else {
           startPromise.fail(http.cause());
         }
