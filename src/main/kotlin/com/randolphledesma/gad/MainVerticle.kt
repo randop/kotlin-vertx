bd07035280f9ce79c430589f136f522b5ab11556
Randolph <randop@me.com>
2020-04-17 14:38:22 +0800

    updates

---
+++ src/main/kotlin/com/randolphledesma/gad/MainVerticle.kt
@@ -0,0 +1,25 @@
+package com.randolphledesma.gad
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+
+class MainVerticle : AbstractVerticle() {
+
+  override fun start(startFuture: Future<Void>) {
+    vertx
+      .createHttpServer()
+      .requestHandler { req ->
+        req.response()
+          .putHeader("content-type", "text/plain")
+          .end("Hello World")
+      }
+      .listen(8888) { http ->
+        if (http.succeeded()) {
+          startFuture.complete()
+          println("HTTP server started on port 8888")
+        } else {
+          startFuture.fail(http.cause());
+        }
+      }
+  }
+}
