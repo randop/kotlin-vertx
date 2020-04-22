bd23947bcb2c6547e49bf92fe976a60a5ce0eca0
Randolph <randop@me.com>
2020-04-22 16:34:25 +0800

    refactor

--- src/main/kotlin/com/randolphledesma/gad/App.kt
+++ src/main/kotlin/com/randolphledesma/gad/App.kt
@@ -1,19 +1,6 @@
 package com.randolphledesma.gad
 
-import kotlinx.coroutines.*
-
 import javax.inject.Inject
-
-import io.vertx.core.Verticle
-import io.vertx.core.DeploymentOptions
-import io.vertx.core.Vertx
-import io.vertx.kotlin.coroutines.awaitResult
-import io.vertx.kotlin.coroutines.dispatcher
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.Promise
-import io.vertx.core.Future
-
-import java.util.*
 
 fun main(args: Array<String>) {
     DaggerApplicationComponent.builder()
@@ -28,24 +15,4 @@ fun main(args: Array<String>) {
             .build()
             .application()
             .start()
-}
-
-class HttpVerticle : AbstractVerticle() {
-  override fun start(startFuture: Future<Void>) {
-    vertx
-      .createHttpServer()
-      .requestHandler { req ->
-        req.response()
-          .putHeader("content-type", "text/plain")
-          .end("Hello from Vert.x!")        
-      }
-      .listen(8888) { http ->
-        if (http.succeeded()) {
-          startFuture.complete()
-          println("HTTP server started on port 8888")
-        } else {
-          startFuture.fail(http.cause());
-        }
-      }
-  }
 }
